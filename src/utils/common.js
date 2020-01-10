8eabfda1f788676e25d0229291191b92e7bb6e36
Dmitriy <d.orlov777@gmail.com>
2020-01-10 13:32:21 +0300

    Правит функции dates/ranges

--- src/utils/common.js
+++ src/utils/common.js
@@ -44,16 +44,20 @@ export const formatDate = (date1, date2) => {
 
 export const getShortDate = (date) => date ? moment(date).format(`D MMM`) : ``;
 
-export const isDateInRange = (data, range) => {
-  return data >= range.from && data <= range.to;
+export const isDateInRange = (range, date) => {
+  return date >= range.from && date <= range.to;
 };
 
-export const isDateInRanges = (ranges, ...data) => {
-  return ranges.some((it) => isDateInRange(data, it));
+export const isDatesInRange = (range, ...dates) => {
+  return dates.every((date) => isDateInRange(range, date));
 };
 
-export const getDataRange = (data, ranges) => {
-  return ranges.find((it) => isDateInRange(data, it));
+export const isDatesInRanges = (ranges, ...dates) => {
+  return ranges.some((range) => isDatesInRange(range, [...dates]));
+};
+
+export const getDatesRange = (ranges, ...dates) => {
+  return ranges.find((range) => isDatesInRange(range, [...dates]));
 };
 
 export const flatDateRanges = (ranges) => {
